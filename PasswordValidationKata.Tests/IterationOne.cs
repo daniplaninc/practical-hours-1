51103249f6cde01574e3dbfbec27d3404faf1b6d
Dani Planinc <dani.planinc@gmail.com>
2026-09-14 14:55:00 +0200

    feat: refactor tests

--- PasswordValidationKata.Tests/IterationOne.cs
+++ PasswordValidationKata.Tests/IterationOne.cs
@@ -2,93 +2,90 @@ namespace PasswordValidationKata.Tests;
 
 public class IterationOne
 {
+    private const string ValidPassword = "hdv_bg6stf.DUSFB";
+    private const string ShortPassword = "v_b.DUB";
+    private const string LowerPassword = "hdv_bg6stf.dusfb";
+    private const string UpperPassword = "HDV_BG6STF.DUSFB";
+    private const string NumberlessPassword = "hdv_bgstf.DUSFB";
+    private const string UnderscorelessPassword = "hdvbg6stf.DUSFB";
+    
     [Fact]
     public void AcceptPasswordWithMoreThanEightCharacters()
     {
-        string longPassword = "hdv_bg6stf.DUSFB";
         var passwordValidator = new PasswordValidator();
         
-        Assert.True(passwordValidator.Validate(longPassword));
+        Assert.True(passwordValidator.Validate(ValidPassword));
     }
     
     [Fact]
     public void RejectPasswordWithLessThanEightCharacters()
     {
-        string shortPassword = "v_bf.DU";
         var passwordValidator = new PasswordValidator();
         
-        Assert.False(passwordValidator.Validate(shortPassword));
+        Assert.False(passwordValidator.Validate(ShortPassword));
     }
     
     [Fact]
     public void AcceptPasswordWithCapitalLetters()
     {
-        string longPassword = "hdv_bg6stf.DUSFB";
         var passwordValidator = new PasswordValidator();
         
-        Assert.True(passwordValidator.Validate(longPassword));
+        Assert.True(passwordValidator.Validate(ValidPassword));
     }
     
     [Fact]
     public void RejectPasswordWithoutCapitalLetters()
     {
-        string longPassword = "hdv_bg6stf.dusfb";
         var passwordValidator = new PasswordValidator();
         
-        Assert.False(passwordValidator.Validate(longPassword));
+        Assert.False(passwordValidator.Validate(LowerPassword));
     }
     
     [Fact]
     public void AcceptPasswordWithLowercaseLetters()
     {
-        string longPassword = "hdv_bg6stf.DUSFB";
         var passwordValidator = new PasswordValidator();
         
-        Assert.True(passwordValidator.Validate(longPassword));
+        Assert.True(passwordValidator.Validate(ValidPassword));
     }
     
     [Fact]
     public void RejectPasswordWithoutLowercaseLetters()
     {
-        string longPassword = "HDV_BG6STF.DUSFB";
         var passwordValidator = new PasswordValidator();
         
-        Assert.False(passwordValidator.Validate(longPassword));
+        Assert.False(passwordValidator.Validate(UpperPassword));
     }
     
     [Fact]
     public void AcceptPasswordWithNumbers()
     {
-        string longPassword = "hdv_bg6stf.DUSFB";
         var passwordValidator = new PasswordValidator();
         
-        Assert.True(passwordValidator.Validate(longPassword));
+        Assert.True(passwordValidator.Validate(ValidPassword));
     }
     
     [Fact]
     public void RejectPasswordWithoutNumbers()
     {
-        string longPassword = "HDV_BGhSTF.DUSFB";
         var passwordValidator = new PasswordValidator();
         
-        Assert.False(passwordValidator.Validate(longPassword));
+        Assert.False(passwordValidator.Validate(NumberlessPassword));
     }
     
     [Fact]
     public void AcceptPasswordWithUnderscores()
     {
-        string longPassword = "hdv_bg6stf.DUSFB";
         var passwordValidator = new PasswordValidator();
         
-        Assert.True(passwordValidator.Validate(longPassword));
+        Assert.True(passwordValidator.Validate(ValidPassword));
     }
     
     [Fact]
     public void RejectPasswordWithoutUnderscores()
     {
-        string longPassword = "hdvbg6stf.DUSFB";
         var passwordValidator = new PasswordValidator();
         
-        Assert.False(passwordValidator.Validate(longPassword));
+        Assert.False(passwordValidator.Validate(UnderscorelessPassword));
     }
 }
